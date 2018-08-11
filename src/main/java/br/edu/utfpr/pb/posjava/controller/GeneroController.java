09c035eb47e8d406b5725b61a23c47dc16eadb96
Denis <d3nys.inf@gmail.com>
2018-08-11 10:05:16 -0300

    Criando Post REST

--- src/main/java/br/edu/utfpr/pb/posjava/controller/GeneroController.java
+++ src/main/java/br/edu/utfpr/pb/posjava/controller/GeneroController.java
@@ -5,13 +5,72 @@
  */
 package br.edu.utfpr.pb.posjava.controller;
 
+import br.com.caelum.vraptor.Consumes;
 import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Get;
+import br.com.caelum.vraptor.Path;
+import br.com.caelum.vraptor.Post;
+import br.com.caelum.vraptor.Result;
+import br.com.caelum.vraptor.serialization.gson.WithoutRoot;
+import static br.com.caelum.vraptor.view.Results.json;
+import static br.com.caelum.vraptor.view.Results.status;
+import br.edu.utfpr.pb.posjava.model.Genero;
+import br.edu.utfpr.pb.posjava.repository.GeneroRepository;
+import br.edu.utfpr.pb.posjava.repository.impl.GeneroRepositoryImpl;
+import javax.inject.Inject;
 
 /**
  *
  * @author denis
  */
 @Controller
+@Path("/genero")
 public class GeneroController {
+
+    private Result result;
+
+    private GeneroRepository generoRepository;
+
+    /**
+     * @deprecated
+     */
+    public GeneroController() {
+    }
+
+    @Inject
+    public GeneroController(Result result, GeneroRepositoryImpl generoRepositoryImpl) {
+        this.result = result;
+        this.generoRepository = generoRepositoryImpl;
+
+    }
+
+    @Get
+    @Path(value = {"", "/"})
+    public void list() {
+
+        result.use(json())
+                .withoutRoot()
+                .from(generoRepository.findAll())
+                .serialize();
+
+    }
     
+    @Get
+    @Path(value =  "/{id}")
+    public void buscar(Long id) {
+
+        result.use(json())
+                .withoutRoot()
+                .from(generoRepository.findOne(id))
+                .serialize();
+
+    }
+    @Post
+    @Path(value = {"/",""})
+    @Consumes(value = "application/json",options = WithoutRoot.class )
+    public void salvar(Genero genero){
+        generoRepository.save(genero);
+        result.use(status()).created();
+    }
+
 }
