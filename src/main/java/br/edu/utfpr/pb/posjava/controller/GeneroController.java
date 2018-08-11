5c7fb509c47862edabbab0951e6b085e4e17054b
Denis <d3nys.inf@gmail.com>
2018-08-11 08:42:45 -0300

    Subindo com Dependecia

---
+++ src/main/java/br/edu/utfpr/pb/posjava/controller/GeneroController.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.utfpr.pb.posjava.controller;
+
+import br.com.caelum.vraptor.Controller;
+
+/**
+ *
+ * @author denis
+ */
+@Controller
+public class GeneroController {
+    
+}
